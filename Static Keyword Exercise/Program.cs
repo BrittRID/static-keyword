d59c0eb0e70d5b902356caab0b31eec1f98a6e5c
Brittany Ridley <britt.ridley@outlook.com>
2021-10-16 20:44:30 -0500

    static keyword

---
+++ Static Keyword Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Static_Keyword_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(10000);//20
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(5); //68
+
+
+            Console.WriteLine("After Conversion");
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
